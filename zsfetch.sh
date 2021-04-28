4cc15e7c6ddf5d87038eaf18973611be7f02c3d4
MrZloHex <zlo.alex.it@gmail.com>
2021-04-28 22:47:58 +0300

    first_steps

--- zsfetch.sh
+++ zsfetch.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 # zsfetch simple POSIX fetch command-line script
 
+
+# making normal name of OS
+decrypt_os() {
+	case $general_os in
+		Linux | GNU* )	 	os=Linux		;;
+
+
+		* )	echo "UNKNOWN OS"
+			echo "Aborting..."
+			exit 1							;;
+
+	esac
+}
+
+
+# getting some standart inforamtion from uname
+get_uname(){
+	IFS=" " read -ra uname <<< "$(uname -srm)"
+
+    general_os="${uname[0]}"
+    distro="${uname[1]}"
+    architecture="${uname[2]}"
+}
+
+
 main() {
-	echo $OSTYPE	
+	get_uname
+	decrypt_os
 }
 
 main "$@"
