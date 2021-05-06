f3e50ef9a892f07e9a624436e0547156476be611
MrZloHex <zlo.alex.it@gmail.com>
2021-05-06 22:17:21 +0300

    man page on mac

--- deployment.sh
+++ deployment.sh
@@ -1,21 +1,56 @@
 #!/bin/bash
 
-install(){
-	sudo mv zsfetch /usr/local/bin
-	sudo mkdir /usr/local/man
-	sudo mkdir /usr/local/man/man1
-	sudo mv zsfetch.1 /usr/local/man/man1
-	sudo gzip /usr/local/man/man1/zsfetch.1
-	sudo mandb
+OS_TYPE="linux"
 
+get_os(){
+	case "$OSTYPE" in 
+		darwin*)
+			OS_TYPE="darwin"
+		;;
+
+		*) 
+			OS_TYPE="linux"
+		;;
+	esac		
+}
+
+install(){
+	# installing program
+	sudo mv zsfetch /usr/local/bin
+
+	# installing man page
+	if [[ $OS_TYPE == "linux" ]]
+	then
+		#sudo mkdir /usr/local/man
+		sudo mkdir /usr/local/man/man1
+		sudo mv zsfetch.1 /usr/local/man/man1
+		sudo gzip /usr/local/man/man1/zsfetch.1
+		sudo mandb
+
+	elif [[ $OS_TYPE == "darwin" ]] 
+	then
+		sudo mkdir /usr/share/man/man1
+		sudo mv zsfetch.1 /usr/share/man/man1
+		sudo gzip /usr/local/man/man1/zsfetch.1
+		sudo man-db
+	fi
 }
 
 uninstall() {
 	sudo rm /usr/local/bin/zsfetch
-	sudo rm /usr/local/man/man1/zsfetch.1.gz
+
+	if [[ $OS_TYPE == "linux" ]]
+	then	
+		sudo rm /usr/share/man/man1/zsfetch.1.gz
+
+	elif [[ $OS_TYPE == "darwin" ]] 
+	then11
+		sudo rm /usr/share/man/man1/zsfetch.1.gz
+	fi
 }
 
 main() {
+	get_os
 	case $1 in
 		"-i" | "--install") 
 			install
